db589e6709ae0879e4a60cc5e1b60779abf385ea
SwethajayaramSompalli <49846067+SwethajayaramSompalli@users.noreply.github.com>
2022-03-30 17:16:04 -0500

    adding template to index.html

--- addproject.py
+++ addproject.py
@@ -6,12 +6,15 @@ from jsonAppend import *
 # append the new project details into the project.json file.
 def projectName():
     return request.form['newprojectname']
+    #return "name"
 
 def projectDetails():
     return request.form['newprojectdetails']
+    #return "Details"
 
 def projectTools():
     return request.form['newprojectTools']
+    #return "tools"
 
 def addnewproject():
     return {
